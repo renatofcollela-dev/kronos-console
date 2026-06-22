01d8ae05a1ff910e48e080140f474c42b0dbf644
renatofcollela-dev <renatofcollela@gmail.com>
2026-06-22 16:36:23 -0400

    KRONOS console: src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
+import App from './App'
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+)
